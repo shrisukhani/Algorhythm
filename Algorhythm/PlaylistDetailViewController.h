cdbb77ae2f6fac05783dea822972cba06c596384
Shridhar Sukhani <shridharsukhani@outlook.com>
2019-03-02 00:51:58 -0600

    Add PlaylistDetailViewController

---
+++ Algorhythm/PlaylistDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorhythm
+//
+//  Created by Shridhar Sukhani on 3/2/19.
+//  Copyright © 2019 Shridhar Sukhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaylistDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Algorhythm/PlaylistDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  PlaylistDetailViewController.m
+//  Algorhythm
+//
+//  Created by Shridhar Sukhani on 3/2/19.
+//  Copyright © 2019 Shridhar Sukhani. All rights reserved.
+//
+
+#import "PlaylistDetailViewController.h"
+
+@interface PlaylistDetailViewController ()
+
+@end
+
+@implementation PlaylistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
