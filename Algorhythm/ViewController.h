2e3e393f204be112436d36d5aa5229f8d9f30b36
Shridhar Sukhani <shridharsukhani@outlook.com>
2019-03-02 00:19:25 -0600

    Initial Commit

---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Shridhar Sukhani on 3/2/19.
+//  Copyright © 2019 Shridhar Sukhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Algorhythm/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Algorhythm
+//
+//  Created by Shridhar Sukhani on 3/2/19.
+//  Copyright © 2019 Shridhar Sukhani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
